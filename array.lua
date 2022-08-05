7f811c9c1452c7321a0a8aa7e39c04639811d505
vinifillmann <vinifillmann08@gmail.com>
2022-08-05 20:41:25 -0300

    Primeiro commit

---
+++ array.lua
@@ -0,0 +1,15 @@
+copas = {
+    1958,
+    1962,
+    1970,
+    1994,
+    2002
+}
+
+-- copas[10] = 2022
+
+-- print(copas[1])
+
+for i in pairs(copas) do
+    print(copas[i])
+end
---
+++ array_keys.lua
@@ -0,0 +1,11 @@
+copas = {
+    [1958] = "Suecia",
+    [1962] = "Chile",
+    [1970] = "Mexico",
+    [1994] = "EUA",
+    [2002] = "Japao e Coreia do sul"
+}
+
+for key, value in pairs(copas) do
+    print("Ano:", key, "Localizacao:", value)
+end
